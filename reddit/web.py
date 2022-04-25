c01681820d50dbcb13c8331667a9415ad98c2de8
Felix・onlix <67185896+nsde@users.noreply.github.com>
2022-04-25 18:33:12 +0200

    :sparkles: Minimal fixes

--- reddit/web.py
+++ reddit/web.py
@@ -18,14 +18,4 @@ def u(name):
     sort = flask.request.args.get('sort') or 'hot'
     return flask.render_template('index.html', posts=reddit.posts(name, sort=sort, is_user=True), sub=reddit.user(name), sort=sort)
 
-# @app.route('/upvote/<sub>/<post>')
-# def upvote(sub, post):
-#     reddit.upvote(post)
-#     return flask.redirect(f'/r/{sub}')
-
-# @app.route('/downvote/<sub>/<post>')
-# def downvote(sub, post):
-#     reddit.downvote(post)
-#     return flask.redirect(f'/r/{sub}')
-
 app.run(port=1111, debug=True)
